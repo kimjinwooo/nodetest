b4344811d59d29b2bc9728b12254934592a96714
assajw <assajw_@naver.com>
2022-03-31 15:24:28 +0900

    진우

--- testDB copy/routes/memberRouter.js
+++
@@ -1,8 +0,0 @@
-
-const memberCtrl = require("../controllers/memberCtrl")
-const router = require("express").Router();
-router.route('/')
-    .get(memberCtrl.getMember)
-    .post(memberCtrl.insertMember)
-
-    module.exports = router;
